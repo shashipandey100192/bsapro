279978ace44f2cd0e654e294267c675286379418
shashi <shashiweb64@gmail.com>
2025-07-22 17:10:06 +0530

    graph dyn

--- src/apps/appmodules/dashboard/Mychartpage.js
+++ src/apps/appmodules/dashboard/Mychartpage.js
@@ -1,69 +1,103 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-const data = [
-    {
-        name: 'Page A',
-        uv: 4000,
-        pv: 2400,
-        amt: 2400,
-    },
-    {
-        name: 'Page B',
-        uv: 3000,
-        pv: 1398,
-        amt: 2210,
-    },
-    {
-        name: 'Page C',
-        uv: 2000,
-        pv: 9800,
-        amt: 2290,
-    },
-    {
-        name: 'Page D',
-        uv: 2780,
-        pv: 3908,
-        amt: 2000,
-    },
-    {
-        name: 'Page E',
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
-    },
-    {
-        name: 'Page F',
-        uv: 2390,
-        pv: 3800,
-        amt: 2500,
-    },
-    {
-        name: 'Page G',
-        uv: 3490,
-        pv: 4300,
-        amt: 2100,
-    },
-];
 
 function Mychartpage() {
+
+    const [item, updateitem] = useState([]);
+    const [sb,usb]=useState([]);
+    const [df,udf]=useState([]);
+
+    const myapi = () => {
+        axios.get('https://dummyjson.com/products').then((d) => {
+            updateitem(d.data.products);
+            console.log(d.data.products);
+            udf(d.data.products);
+
+
+            const searchby = d.data.products.map((s)=>{
+                return s.category
+            });
+            const uniquedata = [...new Set(searchby)];
+            usb(uniquedata);
+
+        })
+    }
+
+    useEffect(() => {
+        myapi();
+    }, [])
+
+
+    const selectopt = (e)=>{
+        console.log(e.target.value);
+        const fdata = df.filter((d)=>{
+            return d.category===e.target.value
+        });
+        updateitem(fdata);
+    }
+
     return (
-        <div className='container-fluid'>
+        <div className='container-fluid mt-3'>
             <div className='row'>
-                <div className='col-12' style={{height:'90vh'}}>
+                <div className='col-md-3'>
+                    <div class="card text-bg-primary mb-3">
+                        <div class="card-body shadow">
+                            <h5 class="card-title">Total Products</h5>
+                            <h1>{item.length}</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-3'>
+                    <div class="card text-bg-warning mb-3">
+                        <div class="card-body shadow">
+                            <h5 class="card-title">Total Products</h5>
+                            <h1>{item.length}</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-3'>
+                    <div class="card text-bg-info mb-3">
+                        <div class="card-body shadow">
+                            <h5 class="card-title">Total Products</h5>
+                            <h1>{item.length}</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-3'>
+                    <div class="card text-bg-danger mb-3">
+                        <div class="card-body shadow">
+                            <h5 class="card-title">Total Products</h5>
+                            <h1>{item.length}</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-3'>
+                    Search By: <select className='form-select' onChange={selectopt}>
+                                <option hidden>search by</option>
+                                {sb.map((lst)=>{
+                                    return <option>{lst}</option>
+                                })}
+                    </select>
+                </div>
+                <div className="col-12">
+                    <hr/>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-12' style={{ height: '80vh' }}>
                     <ResponsiveContainer width="100%" height="100%">
                         <BarChart
                             width={500}
                             height={300}
-                            data={data}
-                        >
-                            {/* <CartesianGrid strokeDasharray="3 3" /> */}
-                            <XAxis dataKey="name" />
+                            data={item}>
+                            <XAxis dataKey="category" />
                             <YAxis />
                             <Tooltip />
                             <Legend />
-                            <Bar dataKey="pv" fill="#fc491cff" activeBar={<Rectangle fill="pink" stroke="blue" />} />
-                            <Bar dataKey="uv" fill="#1dff74ff" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+                            <Bar dataKey="price" fill="#fc491cff" />
+                            <Bar dataKey="stock" fill="#1dff74ff"/>
                         </BarChart>
                     </ResponsiveContainer>
 
